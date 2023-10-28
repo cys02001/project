11ce9f2f4e7a1095c96080a214cded5f2f9d9d14
yunseochoi(home) <jamie0730@naver.com>
2023-10-28 21:01:20 +0900

    이미지 배경 제거 및 골키퍼, 키커, 축구공 화면 띄우기 완료, 움직임 미완

--- background.py
+++ background.py
@@ -15,14 +15,43 @@ class Ai_Keeper:
     def __init__(self):
         self.x, self.y = 400, 300
         self.frame = 0
-        self.image = load_image('ai_keeper.png')
+        self.image = load_image('ai_keeper-removebg-preview.png')
 
     def update(self):
         self.frame = (self.frame + 1) % 2
 
 
     def draw(self):
-        self.image.clip_draw(self.frame * 50, 0, 100, 100, self.x, self.y)
+        self.image.clip_draw(self.frame * 1, 0, 20, 40, self.x, self.y, 80, 200)
+
+
+class Ai_Kicker:
+    def __init__(self):
+        self.x, self.y = 300, 0
+        self.frame = 0
+        self.image = load_image('ai_kicker-removebg-preview.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 2
+
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 1, 0, 30, 80, self.x, self.y, 100, 200)
+
+
+
+class Ball:
+    def __init__(self):
+        self.x, self.y = 400, 20
+        self.frame = 0
+        self.image = load_image('ball21x21.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 2
+
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 1, 0, 80, 80, self.x, self.y,40,40)
 
 
 def handle_events():
@@ -36,15 +65,20 @@ def reset_world():
     global running
     global background
     global ai_keeper
+    global ai_kicker
+    global ball
 
     running = True
     background = Background()
     ai_keeper = Ai_Keeper()
-
+    ai_kicker = Ai_Kicker()
+    ball = Ball()
 
 def update_world():
     background.update()
     ai_keeper.update()
+    ai_kicker.update()
+    ball.update()
     pass
 
 
@@ -52,6 +86,8 @@ def render_world():
     clear_canvas()
     background.draw()
     ai_keeper.draw()
+    ai_kicker.draw()
+    ball.draw()
     update_canvas()
 
 
