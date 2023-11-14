4b5c699bcd47ab0b30ce3229542f05948acb54db
yunseochoi(home) <jamie0730@naver.com>
2023-11-14 17:26:07 +0900

    gauge_bar ui 구현

--- ground.py
+++ ground.py
@@ -4,9 +4,10 @@ from pico2d import load_image
 class Ground:
     def __init__(self):
         self.image = load_image('ground.png')
-
+        global gauge_bar_image
+        gauge_bar_image = load_image('gauge_bar.png')
     def draw(self):
         self.image.draw(400, 300, 800, 600)
-
+        gauge_bar_image.draw(400,100)
     def update(self):
         pass
